e0c8e57cfb482b9b5f98b8b7c307e2c830239783
wilschok <william.schokkele@howest.be>
2017-02-16 20:09:21 +0100

    MileToKm

--- ConsUnitConversion/Program.cs
+++ ConsUnitConversion/Program.cs
@@ -8,8 +8,27 @@ namespace ConsUnitConversion
 {
     class Program
     {
+        delegate double ConvertUnit(double value);
+        
+        
+
+        public double MileToKm(double mile)
+        {
+            return mile * 1.609344;
+            
+        }
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            double input = 20f;
+
+            ConvertUnit cu = new ConvertUnit(p.MileToKm);
+            double km = cu(input);
+            Console.WriteLine($"{input} miles = {Math.Round(km, 3)} km");
+
+
+            Console.ReadKey();
         }
     }
 }
