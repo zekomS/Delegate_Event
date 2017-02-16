2ab51a8fe643d3289fcbb46f4ec3e05f88d2c98f
wilschok <william.schokkele@howest.be>
2017-02-16 20:46:24 +0100

    Cafe Library + Console

---
+++ ConsoleCafe/Program.cs
@@ -0,0 +1,32 @@
+﻿using LibCafe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCafe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPints = 10;
+            PintDish pintDish = new PintDish(numberOfPints);
+
+            for (int i = 0; i < numberOfPints ; i++)
+            {
+                try
+                {
+                    pintDish.AddPint();
+                    Console.WriteLine($"Pint {pintDish.PintCount} added");
+                } catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LibCafe/PintDish.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibCafe
+{
+    public class PintDish
+    {
+        private int pintCount;
+
+        public int PintCount { get { return pintCount; } } // c#6.0 enkel get in property: set enkel in constructor
+        public int MaxPints { get; }
+
+        public PintDish(int maxPints)
+        {
+            MaxPints = maxPints;
+        }
+
+        public void AddPint()
+        {
+            if (pintCount >= MaxPints) throw new Exception("Dish full, order cancelled");
+            pintCount++;
+        }
+    }
+}
